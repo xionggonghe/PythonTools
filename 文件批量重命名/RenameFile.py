ee688d9333e0f7c6d78f6e5b89edc2b000751fb6
lab <1271504766@qq.com>
2023-03-26 10:33:18 +0800

    添加featureMap

--- 文件批量重命名/RenameFile.py
+++ 文件批量重命名/RenameFile.py
@@ -1,17 +1,30 @@
 import os
 
 if __name__ == '__main__':
-    file_dir = "F:/project/pycharm/dataset/Rain100/rain_data_test_Heavy/rain_heavy_test/rain/"
+    file_dir = "F:/project/pycharm/dataset/Rain1400/norain/"
+    endStr = ".jpg"
+
+    type = "rain" if file_dir.find("norain")<0 else "norain"
+
     for root, dirs, files in os.walk(file_dir):
         print('root_dir:', root)  # 当前路径
         print('sub_dirs:', dirs)  # 子文件夹
         print('files:', files)  # 文件名称，返回list类型
     for i in range(len(files)):
-        print("files[i]", files[i])
-        num = files[i][files[i].find("-")+1:files[i].find("x2")]
-        print("num:", num)
+        print("files[{}]".format(i), files[i])
 
-        des = root+"rain-"+num+".png"
+        num = files[i][:files[i].find(endStr)]
+        print("num:", num)
+        # if len(num)<4:
+        #     if len(num)<3:
+        #         if len(num)<2:
+        #             des = root + type +"-000" + num + endStr
+        #         else:
+        #             des = root + type +"-00" + num + endStr
+        #     else:
+        #         des = root + type +"-0" + num + endStr
+        # else:
+        des = root + type + "-" +num+endStr
         print("des", des)
         os.rename(root+files[i], des)
         print(" ")
--- 监测学习率/TestLr.py
+++ 监测学习率/TestLr.py
@@ -1,24 +1,39 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
 
 class models(nn.Module):
     def __init__(self):
         super(models, self).__init__()
-        self.con = nn.Sequential(nn.Conv2d(3, 3, 3, 1, 1))
+        self.con = nn.Sequential(nn.Conv2d(3, 3, 3, 1, 1),
+                                 nn.Conv2d(3, 3, 3, 1, 1),
+                                 nn.Conv2d(3, 3, 3, 1, 1),
+                                 nn.Conv2d(3, 3, 3, 1, 1))
 
     def forward(self, x):
         x = self.con(x)
         return x
 
-NUM_EPOCHS = 30
-warmup_epochs = 3
+NUM_EPOCHS = 300
+# warmup_epochs = 150
 model = models()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=2, T_mult=2)
+
+
+
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+optimizer = optim.Adam(model.parameters(), lr=2e-4, betas=(0.9, 0.999), eps=1e-8)
+
+# scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=2, T_mult=2)
+# scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.96)
 # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer_G, NUM_EPOCHS - warmup_epochs, eta_min=1e-6)
+# scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, NUM_EPOCHS - warmup_epochs, eta_min=1e-7)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.9, patience=10, threshold=0.3,threshold_mode='abs', eps=1e-8)
+
 y = torch.zeros([4, 3, 16, 16])
 loss = nn.L1Loss()
+
+writer = SummaryWriter('runs/experiment_1')
 
 if __name__ == '__main__':
     x = torch.ones([4, 3, 16, 16])
@@ -26,10 +41,17 @@ if __name__ == '__main__':
         optimizer.zero_grad()
         out = model(x)
         loss1 = loss(out, y)
+
         loss1.backward()
+        p = 1 / loss1
         optimizer.step()
         print(optimizer.state_dict()['param_groups'][0]['lr'])
-        scheduler.step()
+        scheduler.step(p)
+
+        writer.add_scalar('lr',
+                          optimizer.state_dict()['param_groups'][0]['lr'],
+                          i)
+
 
 
 
